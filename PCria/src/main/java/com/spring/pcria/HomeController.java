1cea42b61e7208ad5436d0186c72166894bde471
david <david@192.168.0.21>
2020-10-18 16:25:56 +0900

    2020.10.18 인터셉트 작업 중!!

--- PCria/src/main/java/com/spring/pcria/HomeController.java
+++ PCria/src/main/java/com/spring/pcria/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/pcria", method = RequestMethod.GET)
 	public String home(Model model) {
 		
 		return "home";
